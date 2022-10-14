373f4f1ef221ffff5b0c0869ff0b03c29aa16b48
Vito Don Pedro <mrvitodonpedro@gmail.com.com>
2022-10-14 09:17:53 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: ALways 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0); 
+}
