051fe6c37b71029406c5e11fe89b176190eeee18
Vito Don Pedro <mrvitodonpedro@gmail.com.com>
2022-10-19 04:00:18 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,8 +12,9 @@ void print_alphabet_x10(void)
 	for (count = 0; count <= 9; count++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
+
 			_putchar(letter);
 
-		_putchar('/n');
+		_putchar('\n');
 	}
 }
