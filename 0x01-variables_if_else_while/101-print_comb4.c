71aa58dc0fe9803c8b60cf8ab6b530b12299efcd
Vito Don Pedro <mrvitodonpedro@gmail.com.com>
2022-10-20 04:12:42 -0700

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, m, l;
+
+	for (n = 18; n < 28; n++)
+	{
+		for (m = 19; m < 28; m++)
+		{
+			for (l = 20; l < 28; l++)
+			{
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
