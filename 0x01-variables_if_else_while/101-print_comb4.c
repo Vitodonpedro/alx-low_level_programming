f474df90944ca67f2290e5b97d9f2fa48ad6c137
Vito Don Pedro <mrvitodonpedro@gmail.com.com>
2022-10-21 07:33:32 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,35 +7,33 @@
 
 int main(void)
 {
-	int n, m, l;
+	int n = 48;
+	int m, l;
+	int com = 44;
 
-	for (n = 0; n <= 8; n++)
+	while (n <= 58)
 	{
-		for (m = 0; m <= 8; m++)
+		m = n + 1;
+		while (m <= 58)
 		{
-			for (l = 0; l <= 8; l++)
+			l = m + 1;
+			while (l <= 58)
 			{
-				if (l > m && m > n)
+				putchar(n);
+				putchar(m);
+				putchar(l);
+				if (n != 56 && m != 57 && l != 58)
 				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n == 5 && m == 6 && l == 7)
-					{
-						break;
-					}
-					else
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(com);
+					putchar(32);
 				}
+				l += 1;
 			}
+			m += 1;
 		}
+		n += 1;
 	}
 	putchar('\n');
 
 	return (0);
 }
-
-
