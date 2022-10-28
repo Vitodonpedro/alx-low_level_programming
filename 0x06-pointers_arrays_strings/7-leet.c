93afaf51a71166f1b3415b5804da159add5fe774
Vito Don Pedro <mrvitodonpedro@gmail.com.com>
2022-10-28 07:10:10 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - Write a function that encodes a string into 1337
+ * @n: input
+ * Return: the value of n
+ */
+
+char *leet(char *n)
+{
+	int o, p;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (o = 0; n[p] != '\0'; o++)
+	{
+		for (p = 0; p < 10; p++)
+		{
+			if (n[o] == s1[p])
+			{
+				n[o] = s2[p];
+			}
+		}
+	}
+	return (n);
+}
