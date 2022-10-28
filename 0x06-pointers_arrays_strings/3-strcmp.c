e3db21ccacdc2678e8425d94e6206b0555d24255
Vito Don Pedro <mrvitodonpedro@gmail.com.com>
2022-10-28 04:55:45 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,14 +12,14 @@ int _strcmp(char *s1, char *s2)
 {
 	int y;
 
-	y = 0
-		while (s1[y] != '\0' && s2[y] != '\0')
+	y = 0;
+	while (s1[y] != '\0' && s2[y] != '\0')
+	{
+		if (s1[y] != s2[y])
 		{
-			if (s1[y] != s2[y])
-			{
-				return (s1[y] - s2[y]);
-			}
-			y++;
+			return (s1[y] - s2[y]);
 		}
+		y++;
+	}
 	return (0);
 }
