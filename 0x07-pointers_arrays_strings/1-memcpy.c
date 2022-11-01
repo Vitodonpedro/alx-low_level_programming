d6d96b32bbffcebffffac63a8bb3e57a73642796
Vito Don Pedro <mrvitodonpedro@gmail.com.com>
2022-11-01 06:46:08 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - a function that copies memory area
+ * @dest: A pointer that stores copies n bytes from memory area src
+ * @src: The source of the copy
+ * @n: The number og bytes to copy fro, src
+ * Return: Always 0 (Success)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	unsigned char *dest = dest;
+	unsigned char *source = src;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+
+}
