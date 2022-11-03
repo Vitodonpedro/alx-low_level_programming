6fda5ecf1c94f5c19d3f1cd1e5828b06fc35efe1
Vito Don Pedro <mrvitodonpedro@gmail.com.com>
2022-11-03 08:15:08 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Write a function that returns
+ * the natural square root of a number
+ * @i: integer to guess
+ * @n: integer to get root of
+ * Return: value of root
+ */
+
+int _sqrt_recursion(int i, int n)
+{
+	int i = 0;
+
+	if (n == 0 || n == 1)
+		return (n);
+
+	else if (i * i < n)
+		return (_sqrt_recursion(i + 1, n));
+
+	else if (i * i == n)
+		return (i);
+
+	return (-1);
+	return (-1);
+}
