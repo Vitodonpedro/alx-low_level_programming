f2e46abea45269a88afdaddae4f1803705b7f7e1
Vito Don Pedro <mrvitodonpedro@gmail.com.com>
2022-11-10 01:02:29 -0800

    yes

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,20 +12,38 @@
 
 int main(int argc, char *argv[])
 {
-	int a, b;
 
-	if (argc == 1)
+	int i, res = 1;
+
+
+
+	if (argc != 3)
+
 	{
 
-		a = atoi(argv[1]);
+		printf("%s\n", "Error");
 
-		b = atoi(argv[2]);
+		return (1);
 
-		printf("%s\n", a * b);
-
-		return (0);
 	}
-	printf("error\n");
-	return (1);
+
+	else
+
+	{
+
+		for (i = 1; i < argc; i++)
+
+		{
+
+			res *= atoi(argv[i]);
+
+		}
+
+		printf("%d\n", res);
+
+	}
+
+	return (0);
+
 }
 
